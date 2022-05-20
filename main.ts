865df32036569c8843275773cd6ec04ec9016c5b
Kiwiphoenix <98428865+kiwiphoenix364@users.noreply.github.com>
2022-05-20 17:44:56 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -9,11 +9,11 @@ namespace Zoom {
             let precalc = [0]
             precalc = []
             for (let index3 = 0; index3 < 120; index3++) {
-                precalc.push(index3 / size - 30)
+                precalc.push(index3 / size)
             }
             let variable = scene.createRenderable(zLayer, (image: Image, camera: scene.Camera) => {
                 for (let index = 0; index < 160; index++) {
-                    savedx = index / size - 40
+                    savedx = index / size
                     for (let index2 = 0; index2 < 120; index2++) {
                         buf[index2] = image.getPixel(savedx, precalc[index2])
                     }
