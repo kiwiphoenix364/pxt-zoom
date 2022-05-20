8e903644587b02fb3caea7498e0390f32dab3691
Kiwiphoenix <98428865+kiwiphoenix364@users.noreply.github.com>
2022-05-20 16:47:41 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,36 +1,8 @@
-//% color="#83de8a"
-namespace Blur {
+//% color="#f76820"
+namespace Zoom {
     //% block
-    //% block="Apply Blur Filter Using On Game Update With Pixel Size $size"
+    //% block="Zoom In Screen Image Using On Game Update With Pixel Size $size"
     export function SetBlurFilter(size: number) {
-        if (size >= 5) {
-            let zLayer = 0
-            let savedx = 0
-            let buf = Buffer.create(120)
-            let precalc = [0]
-            let var1 = 0
-            let var2 = 0
-            let var3 = 120 / size
-            precalc = []
-            for (let index3 = 0; index3 < var3; index3++) {
-                precalc.push(Math.constrain(index3 * size + size / 2, 0, 119))
-            }
-            let variable = scene.createRenderable(zLayer, (image: Image, camera: scene.Camera) => {
-                for (let index = 0; index < 160; index++) {
-                    savedx = Math.constrain((Math.round(index / size)) * size + size / 2, 0, 159)
-                    for (let index2 = 0; index2 < var3; index2++) {
-                        var1 = index2 * size
-                        var2 = image.getPixel(savedx, precalc[index2])
-                        for (let index3 = 0; index3 < size; index3++) {
-                            buf[var1 + index3] = var2
-                        }
-                    }
-                    image.setRows(index, buf)
-                }
-            }
-            )
-            control.runInParallel(() => variable.destroy())
-        } else {
             let zLayer = 0
             let savedx = 0
             let buf = Buffer.create(120)
@@ -51,5 +23,4 @@ namespace Blur {
             )
             control.runInParallel(() => variable.destroy())
         }
-    }
 }
