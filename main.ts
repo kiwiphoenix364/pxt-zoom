00a9d7afcbb5442018bb9983ef407764c0dc24ae
Kiwiphoenix <98428865+kiwiphoenix364@users.noreply.github.com>
2022-05-23 16:32:42 -0400

    Restore '5/23/2022, 3:11:15 PM 1.0.0'

--- main.ts
+++ main.ts
@@ -30,43 +30,4 @@ namespace Zoom {
             )
             control.runInParallel(() => variable.destroy())
         }
-    //% block
-    //% block="Zoom In Screen Image From %size Times To %size2 Times Over %time ms"
-    export function SetZoomFilterFrom__To__(size: number, size2: number, time: number) {
-        control.runInParallel(() => {
-        let dif = size2 - size
-        let realsize = size
-        let zLayer = 0
-        let savedx = 0
-        let buf = Buffer.create(120)
-        let precalc = [0]
-        let precalc2 = [0]
-        precalc = []
-        precalc2 = [] 
-        for (let repeat = 0; repeat < time / 50; repeat++) {
-        realsize += repeat * dif / (time / 50)
-        let variable = scene.createRenderable(zLayer, (image: Image, camera: scene.Camera) => {
-            let screenclone = image.clone()
-            let left = (screen.width - screen.width / realsize) / 2
-            let top = (screen.height - screen.height / realsize) / 2
-            for (let index = 0; index < 160; index++) {
-                precalc2.push(Math.floor(index / realsize) + left)
-            }
-            for (let index3 = 0; index3 < 120; index3++) {
-                precalc.push(Math.floor(index3 / realsize) + top)
-            }
-            for (let index5 = 0; index5 < 160; index5++) {
-                for (let index6 = 0; index6 < 120; index6++) {
-                    buf[index6] = screenclone.getPixel(precalc2[index5], precalc[index6])
-                }
-                image.setRows(index5, buf)
-            }
-        
-        }
-        )
-        pause(50)
-        control.runInParallel(() => variable.destroy())
-        }
-        }
-        )}
 }
