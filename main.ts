6657db5f4f02c018afd8802d4067730aa5901f05
Kiwiphoenix <98428865+kiwiphoenix364@users.noreply.github.com>
2022-09-25 23:56:16 -0400

    Update pxt.json, main.ts, test.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,26 @@
+let size1 = 1
+let x1 = 1
+x1 = 0
+let y1 = 1
+y1 = 0
+let buf = Buffer.create(120)
+let zLayer = 1
+let savedx = 1
+let multnum = 1
+let left = 1
+let top = 1
+let variable = scene.createRenderable(zLayer, (image: Image, camera: scene.Camera) => {
+    left = ((screen.width - screen.width / size1) / 2) + x1
+    top = ((screen.height - screen.height / size1) / 2) + y1
+    multnum = 1 / size1
+    let screenclone = image.clone()
+    for (let index5 = 0; index5 < 160; index5++) {
+        for (let index6 = 0; index6 < 120; index6++) {
+            buf[index6] = screenclone.getPixel(multnum * index5 + left, multnum * index6 + top)
+        }
+        image.setRows(index5, buf)
+    }
+})
 enum Mode {
     //% block="Center"
     Center,
@@ -67,33 +90,8 @@ namespace Zoom {
     //% block
     //% block="Zoom In Screen Image By %size Times Using On Game Update With Offset x $x y $y"
     export function SetZoomFilterOffset(size: number, x: number, y: number) {
-        let zLayer = 0
-        let savedx = 0
-        let buf = Buffer.create(120)
-        let precalc = [0]
-        let precalc2 = [0]
-        precalc = []
-        precalc2 = []
-        let variable = scene.createRenderable(zLayer, (image: Image, camera: scene.Camera) => {
-            let screenclone = image.clone()
-            let left = 0
-            let top = 0
-            left = ((screen.width - screen.width / size) / 2) + x
-            top = ((screen.height - screen.height / size) / 2) + y
-            for (let index = 0; index < 160; index++) {
-                precalc2.push((index / size) + left)
-            }
-            for (let index3 = 0; index3 < 120; index3++) {
-                precalc.push((index3 / size) + top)
-            }
-            for (let index5 = 0; index5 < 160; index5++) {
-                for (let index6 = 0; index6 < 120; index6++) {
-                    buf[index6] = screenclone.getPixel(precalc2[index5], precalc[index6])
-                }
-                image.setRows(index5, buf)
-            }
+            size1 = size
+            x1 = x
+            y1 = y
         }
-        )
-        control.runInParallel(() => variable.destroy())
-    }
 }
