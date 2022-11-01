1ed488f9f8798e44b407c9f837d4d39b5be69aee
Kiwiphoenix <98428865+kiwiphoenix364@users.noreply.github.com>
2022-11-01 17:51:35 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -8,9 +8,9 @@ let left = 1
 let top = 1
 let variable = scene.createRenderable(zLayer, (image: Image, camera: scene.Camera) => {
     let screenclone = image.clone()
-    helpers.imageBlit(image, 16 % (size1) - 16, 12 % (size1) - 16, 176, 136, screenclone, x1, y1, 160 / size1, 120 / size1, true, false)
+    //helpers.imageBlit(image, (x1 - Math.trunc(x1)) * - size1, (y1 - Math.trunc(y1)) * - size1, 160 + Math.ceil(size1), 120 + Math.ceil(size1), screenclone, x1, y1, (160 - Math.ceil(size1)) / size1, (120 - Math.ceil(size1)) / size1, true, false)
+    helpers.imageBlit(image, x1, y1, 160 * size1, 120 * size1, screenclone, 0, 0, 159, 119, true, false)
 })
-
 enum Mode {
     //% block="Center"
     Center,
@@ -40,32 +40,30 @@ namespace Zoom {
     //% picker=Mode
     //% block="zoom in screen image to %size times with anchor $anchor over $ms ms"
     export function SetZoomFilter(size: number, anchor: Mode, ms: number) {
-            size1 = size
-            if (anchor == 0 || anchor == 2 || anchor == 7) {
-                x1 = (160 - (160 / size1)) / 2
-            } else if (anchor == 3 || anchor == 5 || anchor == 8) {
-                x1 = 160 - (160 / size1)
-            }
-            if (anchor == 0 || anchor == 4 || anchor == 5) {
-                y1 = (120 - (120 / size1)) / 2
-            } else if (anchor == 6 || anchor == 7 || anchor == 8) {
-                y1 = 120 - (120 / size1)
-            }
-            memsize = size1
+            memsize = size - size1
             for (let i = 0; i < ms / (25); i++) {
                 size1 = size1 + memsize / (ms / 25)
+                if (anchor == 0 || anchor == 2 || anchor == 7) {
+                    x1 = (160 - (160 / size1)) / 2
+                } else if (anchor == 3 || anchor == 5 || anchor == 8) {
+                    x1 = 160 - (160 / size1)
+                }
+                if (anchor == 0 || anchor == 4 || anchor == 5) {
+                    y1 = (120 - (120 / size1)) / 2
+                } else if (anchor == 6 || anchor == 7 || anchor == 8) {
+                    y1 = 120 - (120 / size1)
+                }
                 pause(25)
             }
         }
     //% block
     //% block="zoom in screen image to %size times with offset x $x y $y over $ms ms"
     export function SetZoomFilterOffset(size: number, x: number, y: number, ms: number) {
-            
             memsize = size - size1
             for (let i = 0; i < ms / (25); i++) {
                 size1 = size1 + memsize / (ms / 25)
-                x1 = x + (160 - (160 / size1)) / 2
-                y1 = y + (120 - (120 / size1)) / 2
+                x1 = x + 80 - size1 * 80 - ((160 - (160 / size1)) / 2)
+                y1 = y + 60 - size1 * 60 - ((120 - (120 / size1)) / 2)
                 pause(25)
             }
         }
