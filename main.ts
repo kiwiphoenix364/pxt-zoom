268db98aefb48dd0d33ad8fc7876627fa7986f6a
Kiwiphoenix <kiwiphoenix364@gmail.com>
2023-02-08 16:17:25 -0500

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -4,11 +4,13 @@ let x1 = 0
 let memsize1 = 0
 let size1 = 1
 let zLayer1 = 1
+let sw = screen.width
+let sh = screen.height
 let variable = scene.createRenderable(zLayer1, (image: Image, camera: scene.Camera) => {
     let screenclone = image.clone()
-    //helpers.imageBlit(image, (x1 - Math.trunc(x1)) * - size1, (y1 - Math.trunc(y1)) * - size1, 160 + Math.ceil(size1), 120 + Math.ceil(size1), screenclone, x1, y1, (160 - Math.ceil(size1)) / size1, (120 - Math.ceil(size1)) / size1, true, false)
-    image.fillRect(0, 0, 160, 120, 0)
-    helpers.imageBlit(image, x1, y1, 160 * size1, 120 * size1, screenclone, 0, 0, 160, 120, true, false)
+    //helpers.imageBlit(image, (x1 - Math.trunc(x1)) * - size1, (y1 - Math.trunc(y1)) * - size1, sw + Math.ceil(size1), sh + Math.ceil(size1), screenclone, x1, y1, (sw - Math.ceil(size1)) / size1, (sh - Math.ceil(size1)) / size1, true, false)
+    image.fillRect(0, 0, sw, sh, 0)
+    helpers.imageBlit(image, x1, y1, sw * size1, sh * size1, screenclone, 0, 0, sw, sh, true, false)
 })
 enum Mode {
     //% block="Center"
@@ -50,14 +52,14 @@ namespace Zoom {
             for (let i = 0; i < (ms / 25); i++) {
                 size1 += memsize1 / (ms / 25)
                 if (anchor == 0 || anchor == 2 || anchor == 7) {
-                    x1 = 80 - 80 * size1
+                    x1 = (sw / 2) - (sw / 2) * size1
                 } else if (anchor == 3 || anchor == 5 || anchor == 8) {
-                    x1 = 160 - (160 * size1)
+                    x1 = sw - (sw * size1)
                 }
                 if (anchor == 0 || anchor == 4 || anchor == 5) {
-                    y1 = 60 - 60 * size1
+                    y1 = (sh / 2) - (sh / 2) * size1
                 } else if (anchor == 6 || anchor == 7 || anchor == 8) {
-                    y1 = 120 - (120 * size1)
+                    y1 = sh - (sh * size1)
                 }
                 pause(25)
             }
@@ -74,8 +76,8 @@ namespace Zoom {
             memsize1 = size - size1
             for (let j = 0; j < (ms / 25); j++) {
                 size1 += memsize1 / (ms / 25)
-                x1 = -x + 80 - size1 * 80
-                y1 = -y + 60 - size1 * 60
+                x1 = -x + (sw / 2) - size1 * (sw / 2)
+                y1 = -y + (sh / 2) - size1 * (sh / 2)
                 pause(25)
             }
         }
